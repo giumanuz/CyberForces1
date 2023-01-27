d993f5a95ea3461f8c353eeb2fb6e6e9d046e1b1
giumanuz <88536667+giumanuz@users.noreply.github.com>
2023-01-27 22:23:41 +0100

    commit

--- script.js
+++ script.js
@@ -18,51 +18,18 @@ function login() {
         alert("Nome utente o password non validi.");
     }
 }
-      
-function uploadFile() {
-    var file = document.getElementById("file").files[0];
-    var formData = new FormData();
-    formData.append("file", file);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "upload.php", true);
-    xhr.send(formData);
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4 || xhr.status !== 200) {
-        //alert("File caricato con successo!");
-        } else {
-        alert("Errore durante il caricamento del file.");
-        }
+function submit() { 
+// parse data from submit.html
+    // var select = document.getElementById('select');
+    // select. = function() {
+    //     alert(this.value);
+    // };
+    window.onload = function() {
+        var select = document.getElementById('select');
+        select.onchange = function() {
+            alert(this.value);
+        };
     }
+    
 }
 
-function uploadText() {
-    var text = document.getElementById("text").value;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "uploadText.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("text=" + text);
-}
-
-function uploadSelect() {
-    var select = document.getElementById("select");
-    var value = select.options[select.selectedIndex].value;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "uploadSelect.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send("value=" + value);
-}
-
-function submit() {
-    uploadSelect(); 
-}
-
-function showPassword() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-    x.type = "text";
-    } else {
-    x.type = "password";
-  }
-} 
